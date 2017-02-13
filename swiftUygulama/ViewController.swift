7bd8a6a59b4beddbb1d13b11823700405bce2b7d
mertcanli <mert_canlli@hotmail.com>
2017-02-13 16:15:15 +0200

    özet

--- swiftUygulama/ViewController.swift
+++ swiftUygulama/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmented: UISegmentedControl!
+    @IBOutlet weak var textview: UITextView!
+    @IBOutlet weak var textfield: UITextField!
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        label.text = "merhaba"
+        
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func button(_ sender: Any) {
+        label.text = textfield.text
+        textview.text=textfield.text
+        
+        switch segmented.selectedSegmentIndex {
+        case 0:
+            label.text="1.seçildi"
+        case 1:
+            label.text="2.seçildi"
+        case 2:
+            label.text="3. seçildi"
+        case 3:
+            label.text="4.seçildi"
+        default:
+            label.text="hata"
+        }
     }
-
-
 }
 
